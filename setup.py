1182d220400813de060a777b37f323ba8618ac22
TerenceLiu98 <terenceliu1012@gmail.com>
2021-05-29 17:27:09 +0800

    v1.0b0

--- setup.py
+++ setup.py
@@ -2,8 +2,8 @@ from setuptools import setup, find_packages
 import os
 setup(
     name = "CEDApy",
-    version = "0.2-beta",
-    keywords = "economic data",
+    version = "1.0-beta",
+    keywords = "quantitative economic data",
     long_description = open(
     os.path.join(
         os.path.dirname(__file__),
@@ -14,10 +14,23 @@ setup(
     author_email = "terenceliu1012@outlook.com",
     url = "https://github.com/TerenceLiu98/CEDApy",
     packages = find_packages(),
+    install_requires=[
+        "matplotlib>=3.1.1",
+        "numpy>=1.15.4",
+        "pandas>=0.25",
+        "requests>=2.22.0",
+        "demjson>=2.2.4",
+        "html5lib>=1.0.1",
+        "xlrd==1.2.0",
+    ],
     license = "MIT",
     classifiers=[
-        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
+    python_requires=">=3.6",
 )
